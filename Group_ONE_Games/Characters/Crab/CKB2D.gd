145f4dfcd49d9abb198316f2fd98716b949996a5
Ezra Dudden <ezradudden@gmail.com>
2019-03-05 14:17:13 -0700

    Adding Collisions to main

--- Group_ONE_Games/Characters/Crab/CKB2D.gd
+++ Group_ONE_Games/Characters/Crab/CKB2D.gd
@@ -1,11 +1,11 @@
-extends KinematicBody2D
+extends "res://Characters/Creatures.gd"
 
 var velocity = Vector2(0,0) #Where it's going
 var anitime #animation time
 var debug = true
-var speed =100
 
 func _ready():
+	setcreature("crab")
 	velocity.x = 1
 	velocity.y = 0
 	anitime = 0.0
@@ -20,15 +20,19 @@ func set_direction():
 	velocity.y = y
 	
 
+
 func _physics_process(delta):
 	# This does collisons and tests if the crab collides witha anything
 	if velocity.x == 0:
 		set_direction(delta)
-	var thing = self.move_and_collide(velocity.normalized() * delta * speed)
+		
+	var thing = move_and_collide(velocity.normalized() * delta * speed)
 	
 	if thing != null: #if we collide with somethign  
 		set_direction()
-	
+		var  obj = instance_from_id(thing.collider_id)
+		if obj.has_method("damage"):
+			obj.damage("crab", 2)
 	
 	
 	# Flip the sprite for animations.
