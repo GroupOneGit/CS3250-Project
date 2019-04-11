aa0d896f490a6c6c1e386ffe366ca19de93a018a
Adriana <flortorres86@gmail.com>
2019-04-10 22:19:15 -0600

    Start scene and Game over seen updated

--- Group_ONE_Games/Scripts/MainMenu.gd
+++ Group_ONE_Games/Scripts/MainMenu.gd
@@ -8,3 +8,12 @@ func _on_NewGameButton_pressed():
 
 func _on_QuitButton_pressed():
 	Global_Player.switch_to("quit")
+
+
+func _on_Start_pressed():
+	Global_Player.full_health()
+	Global_Player.switch_to("main")
+
+
+func _on_Quit_pressed():
+	Global_Player.switch_to("quit")
