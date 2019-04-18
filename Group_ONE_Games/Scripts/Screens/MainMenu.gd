f12ae79604e62e971ac1b008dc45b1629d303fe5
Adriana <flortorres86@gmail.com>
2019-04-18 14:12:24 -0600

    Art work, opening and ending scenes, fix to ending scene.

--- Group_ONE_Games/Scripts/Screens/MainMenu.gd
+++ Group_ONE_Games/Scripts/Screens/MainMenu.gd
@@ -1,15 +1,5 @@
 extends Control
 
-
-func _on_NewGameButton_pressed():
-	Global_Player.full_health()
-	Global_Player.switch_to("main")
-
-
-func _on_QuitButton_pressed():
-	Global_Player.switch_to("quit")
-
-
 func _on_Start_pressed():
 	Global_Player.full_health()
 	Global_Player.switch_to("main")
@@ -17,3 +7,4 @@ func _on_Start_pressed():
 
 func _on_Quit_pressed():
 	Global_Player.switch_to("quit")
+
