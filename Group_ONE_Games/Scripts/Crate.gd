66f68ba6d6b06546e1d414015b5a06a5df602eed
Andy <jjohn509@msudenver.edu>
2019-04-30 12:13:57 -0600

    Added Scrolls to Inventory & interact-able scroll
    also resized Item art, fish, clam, and a few others.
    fixed animations for trap pole

--- Group_ONE_Games/Scripts/Crate.gd
+++ Group_ONE_Games/Scripts/Crate.gd
@@ -11,19 +11,18 @@ func _ready():
 func _on_Area2D_body_entered(body):
 	if body.is_in_group("Player"):
 		inArea = true
-		print("Can Open CRatE")
 
 	
 func _on_Area2D_body_exited(body):
 	if body.is_in_group("Player"):
 		inArea = false
-		print("NAH DUDE")
+
 		
 func _input(event):
 	if event.is_action_pressed("ui_interact") && inArea == true:
 		print("Can Open CRatE")
 		if inventory.inventoryTotal >= 28:
-			print("INVENTORY FULL BUD!")
+			return
 		else:
 			var lootList = inventory.createCrate()
 			for loot in lootList:
