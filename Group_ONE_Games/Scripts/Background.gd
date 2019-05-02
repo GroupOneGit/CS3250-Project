c4757c03f52fa89c5378b0953a075519823f7e28
Andy <jjohn509@msudenver.edu>
2019-05-02 17:57:03 -0600

    Cleared Unused Files
    deleted 500mb of unused files, mainly .mp3s

---
+++ Group_ONE_Games/Scripts/Background.gd
@@ -0,0 +1,11 @@
+extends NinePatchRect
+
+func _ready():
+	pass
+
+func can_drop_data(position, data):
+	return true
+	
+func drop_data(position, data):
+	ItemDatabase.holdingItem = false
+	ItemDatabase.heldItem = null
