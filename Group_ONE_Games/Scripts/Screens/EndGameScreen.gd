68e596d03b34c785dd856d891130d2167488de27
Andy <jjohn509@msudenver.edu>
2019-05-06 20:35:07 -0600

    Minor bug fixes
    fixed if character died with hat, would start new game with hat

--- Group_ONE_Games/Scripts/Screens/EndGameScreen.gd
+++ Group_ONE_Games/Scripts/Screens/EndGameScreen.gd
@@ -2,6 +2,10 @@ extends Control
 
 func _on_Yes_pressed():
 	Global_Player.full_health()
+	Global_Player.has_Hat = false
+	ItemDatabase.restart()
+	for i in range(1,9):
+		ItemDatabase.scroll[i].hasfound = false
 	Global_Player.switch_to("main")
 	pass # Replace with function body.
 
